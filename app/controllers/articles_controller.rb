e17cebc29be1648e90715c896f0989eb76f581db
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2017-12-04 04:44:03 +0600

    New controller action 'show' for permalink to show/read an article
    New erb template for 'show' action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(allowed_article_params)
     @article.save
