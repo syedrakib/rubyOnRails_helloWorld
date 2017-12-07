33adaf6e361655eb1b90e1ae8dcff2577caea630
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2017-12-07 18:54:03 +0600

    Edit/Update an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(allowed_article_params)
     if @article.save
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(allowed_article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
