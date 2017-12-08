ead2ad522be9f9e23d1975f0fa9cd2d6ce8bb7b7
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2017-12-08 07:20:09 +0600

    Add a Comment form and handle creating new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(allowed_comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def allowed_comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
